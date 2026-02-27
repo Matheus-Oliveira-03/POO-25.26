8e40c5138e5bfa8264ff7401796ea69b9eccf79a
LemonBoy <107313086+Matheus-Oliveira-03@users.noreply.github.com>
2026-02-27 17:23:13 +0000

    Finished all the codes for problems D and E

--- LAB2/problem_e/Cliente.java
+++ LAB2/problem_e/Cliente.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
  * @author Matheus Martins Oliveira | a85794
  * @version 1.0 | 27/02/2026
  */
-public class Cliente {
+public class ClienteE {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
 
         Scanner scanner = new Scanner(System.in);
-        Ponto p = new Ponto(scanner.nextFloat(), scanner.nextFloat());
-        Vetor v = new Vetor(scanner.nextFloat(), scanner.nextFloat());
-        SegmentoReta sr = new SegmentoReta(p, v);
-        System.out.println(sr.toString());
+        Ponto p = new Ponto(scanner.nextDouble(), scanner.nextDouble());
+        Ponto q = new Ponto(scanner.nextDouble(), scanner.nextDouble());
+        Vetor v = new Vetor(scanner.nextDouble(), scanner.nextDouble());
+
+        SegmentoReta sr = new SegmentoReta(p, q);
+        System.out.println(sr.intersect(v));
+
         scanner.close();
     }
 }
