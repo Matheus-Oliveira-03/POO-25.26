6c4cab73461ba18a9ee99793c3735f0681ff9438
LemonBoy <107313086+Matheus-Oliveira-03@users.noreply.github.com>
2026-03-23 11:45:53 +0000

    Fineshed problem I, now Im going to problem J

---
+++ LAB4/problem_j/src/Point.java
@@ -0,0 +1,116 @@
+import java.util.function.BinaryOperator;
+
+/**
+ * Represents a point with (x,y) coordinates
+ * 
+ * @author Matheus Martins Oliveira | a85794
+ * @version 3.0 | 27/02/2026
+ */
+public class Point {
+    public static Point ORIGIN = new Point(0, 0);
+    private double x;
+    private double y;
+
+    /**
+     * Constructor from two doubles
+     * 
+     * @param x (double) x axis value
+     * @param y (double) y axis value
+     */
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return a {@code double} representing this point's x axis value
+     */
+    public double x() {
+        return this.x;
+    }
+
+    /**
+     * @return a {@code double} representing this point's y axis value
+     */
+    public double y() {
+        return this.y;
+    }
+
+    /**
+     * @param x        an {@code double} representing a scalar
+     * @param operator an {@code BinaryOperator<Double>} representing the operation
+     *                 between the scalar and this
+     * @return a {@code Point} resulting of the scalar being aplied by the operation
+     *         to each of this point values
+     */
+    public Point calculator(double x, BinaryOperator<Double> operator) {
+        return calculator(new Point(x, x), operator);
+    }
+
+    /**
+     * @param p        an {@code Point} representing the second point for the
+     *                 operation
+     * @param operator an {@code BinaryOperator<Double>} representing the operation
+     *                 between the two points
+     * @return a {@code Point} in which each axis contain the result of the first
+     *         point({@code this}) being aplied by the operation to the second point
+     *         in the same
+     *         axis:{@code (this.x() _operator_ p.x(), this.() _operator_ p.y())}
+     */
+    public Point calculator(Point p, BinaryOperator<Double> operator) {
+        if (operator == null)
+            return null;
+
+        double xx = operator.apply(this.x(), p.x());
+        double yy = operator.apply(this.y(), p.y());
+        return new Point(xx, yy);
+    }
+
+    /**
+     * @param p an {@code Point} representing the point which will be measured the
+     *          distance to
+     * @return a {@code double} representing the distance between this and the point
+     *         p
+     */
+    public double distanceTo(Point p) {
+        return Math.sqrt(Math.pow(this.x - p.x(), 2) + Math.pow(this.y - p.y(), 2));
+    }
+
+    /**
+     * @return a {@code double} representing the distance between this and the
+     *         origin (0, 0)
+     */
+    public double norm() {
+        return distanceTo(ORIGIN);
+    }
+
+    /**
+     * @param p an {@code Point} representing the point which will be compared to
+     *          this
+     * @return {@code true} if the points are equal to each other (this.x() ==
+     *         p-x(), this.y() == p.y())
+     */
+    public boolean equals(Point p) {
+        double xx = Math.abs(this.x() - p.x());
+        double yy = Math.abs(this.y() - p.y());
+
+        return xx < 1e-9 && yy < 1e-9;
+    }
+
+    /**
+     * @param p an {@code Point} representing the point which will be compared to
+     *          this one regarding the distance to the origin (0, 0)
+     * @return {@code -1} If the given point p is closer to the origin<br>
+     *         {@code 0} If both are at the same distance to the origin<br>
+     *         {@code 1} If this is closer to the origin
+     */
+    public int compareTo(Point p) {
+        double disA = this.norm();
+        double disB = p.norm();
+        return Math.abs(disA - disB) < 1e-9 ? 0 : disA < disB ? 1 : -1;
+    }
+
+    public String toString() {
+        return String.format("(%.2f,%.2f)", this.x(), this.y());
+    }
+}
